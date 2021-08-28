43d26cc21d75d26f0c0ca11478ec56f001cf9d07
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-28 14:53:18 -0300

    add anotation

--- src/main/java/com/pokemon/api/service/Interface/IPokemon.java
+++ src/main/java/com/pokemon/api/service/Interface/IPokemon.java
@@ -2,10 +2,13 @@ package com.pokemon.api.service.Interface;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.pokemon.api.dto.PokemonBasicDto;
 import com.pokemon.api.dto.PokemonPlusInfoDto;
 import com.pokemon.api.model.pokemons.InfoPokemons;
 
+@Component
 public interface IPokemon {
 
 	InfoPokemons getPokemonsInfo(int limit, int offset);
