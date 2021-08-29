a33f4b9ac7fa3a08445995a14b6393d381f00fa8
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-29 05:00:54 -0300

    update security

--- src/main/java/com/pokemon/api/config/WebSecConf.java
+++ src/main/java/com/pokemon/api/config/WebSecConf.java
@@ -3,11 +3,10 @@ package com.pokemon.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecConf extends WebSecurityConfigurerAdapter{
+public class WebSecConf {
 
 	
 	 @Bean
@@ -15,7 +14,8 @@ public class WebSecConf extends WebSecurityConfigurerAdapter{
 	        http.requiresChannel().anyRequest().requiresSecure();
 	        return http.build();
 	    }
-	  @Override
+	 
+	 
 	  protected void configure(HttpSecurity http) throws Exception {
 	    http.requiresChannel()
 	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
