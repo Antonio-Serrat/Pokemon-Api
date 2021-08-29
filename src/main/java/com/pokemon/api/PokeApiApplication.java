310f728ec2d68cc0ba7a3d536c574e2a4ce946cd
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-29 04:48:17 -0300

    fix main

--- src/main/java/com/pokemon/api/PokeApiApplication.java
+++ src/main/java/com/pokemon/api/PokeApiApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MainApplication {
+public class PokeApiApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+		SpringApplication.run(PokeApiApplication.class, args);
 	}
 
 }
