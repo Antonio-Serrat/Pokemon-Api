c192c7f98a8373cc1d17c91d68ac5695d2a0d7e0
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-29 11:39:45 -0300

    pom

--- src/main/java/com/pokemon/api/config/WebSecConf.java
+++ src/main/java/com/pokemon/api/config/WebSecConf.java
@@ -1,29 +1,26 @@
 package com.pokemon.api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecConf {
 
 	
-	 @Bean
-	 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	        http.requiresChannel().anyRequest().requiresSecure();
-	        return http.build();
-	    }
+	//	 @Bean
+	//	 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	//	        http.requiresChannel().anyRequest().requiresSecure();
+	//	        return http.build();
+	//	    }
+	//	 
 	 
-	 
-	  protected void configure(HttpSecurity http) throws Exception {
-	    http.requiresChannel()
-	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-	      .requiresSecure();
-	    
-	    
-	    http.authorizeRequests().antMatchers("/api/**").permitAll().anyRequest()
-	    .authenticated();
-	  }
+//	  protected void configure(HttpSecurity http) throws Exception {
+//	    http.requiresChannel()
+//	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//	      .requiresSecure();
+//	    
+//	    
+//	    http.authorizeRequests().antMatchers("/api/**").permitAll().anyRequest()
+//	    .authenticated();
+//	  }
 	}
 
