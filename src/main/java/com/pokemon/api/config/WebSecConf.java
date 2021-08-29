f7125741c55db2071e787c5a13431a4001117218
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-29 13:38:01 -0300

    comment stack trace error stop for a moment

--- src/main/java/com/pokemon/api/config/WebSecConf.java
+++ src/main/java/com/pokemon/api/config/WebSecConf.java
@@ -1,26 +1,22 @@
-//package com.pokemon.api.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class WebSecConf {
-//
-//	
-//		 @Bean
-//		 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//		        http.requiresChannel().anyRequest().requiresSecure();
-//		        return http.build();
-//		    }
-//		 
-//	 
-//	  protected void configure(HttpSecurity http) throws Exception {
-//	    http.requiresChannel()
-//	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-//	      .requiresSecure();
-//	    
-//	    
-//	    http.authorizeRequests().antMatchers("/api/**").permitAll().anyRequest()
-//	    .authenticated();
-//	  }
-//	}
-//
+package com.pokemon.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecConf extends WebSecurityConfigurerAdapter{
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.requiresChannel()
+	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+	      .requiresSecure();
+	    
+	    
+	    http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+	    .authenticated();
+	  }
+	}
+
