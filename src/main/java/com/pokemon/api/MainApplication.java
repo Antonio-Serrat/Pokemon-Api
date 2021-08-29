7f00e79983582aa76055b20f40dd78f24598e68b
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-29 03:09:27 -0300

    Added a Procfile

--- src/main/java/com/pokemon/api/MainApplication.java
+++ src/main/java/com/pokemon/api/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokeApiApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PokeApiApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
--- src/test/java/com/pokemon/api/MainApplicationTests.java
+++ src/test/java/com/pokemon/api/MainApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PokeApiApplicationTests {
+class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
