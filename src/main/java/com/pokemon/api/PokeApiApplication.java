52bfa16b7e4cd8966d1a660bdfd377f02d8f7a93
ToniSrt <4antoniogabriel20@gmail.com>
2021-08-30 11:03:43 -0300

    dependency cache

--- src/main/java/com/pokemon/api/PokeApiApplication.java
+++ src/main/java/com/pokemon/api/PokeApiApplication.java
@@ -2,9 +2,11 @@ package com.pokemon.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@EnableCaching
 public class PokeApiApplication {
 	
 	public static void main(String[] args) {
